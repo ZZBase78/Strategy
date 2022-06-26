ef815ec3ca5dedab03a337c38297bd038cd6cb2d
ZZBase78 <88794134+ZZBase78@users.noreply.github.com>
2022-06-26 23:49:34 +0300

    Сделан заглушечный интерфейс для правой части

---
+++ Assets/Scripts/UserControlSystem/UI/Presenter/BottomRightPresenter.cs
@@ -0,0 +1,38 @@
+﻿using Abstractions;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UserControlSystem
+{
+    public sealed class BottomRightPresenter : MonoBehaviour
+    {
+        [SerializeField] private GameObject _buttonMoveGameObject;
+        [SerializeField] private GameObject _buttonAttackGameObject;
+        [SerializeField] private GameObject _buttonPatrolGameObject;
+        [SerializeField] private GameObject _buttonHoldGameObject;
+
+        [SerializeField] private Button _buttonMove;
+        [SerializeField] private Button _buttonAttack;
+        [SerializeField] private Button _buttonPatrol;
+        [SerializeField] private Button _buttonHold;
+
+        [SerializeField] private SelectableValue _selectedValue;
+
+        private void Start()
+        {
+            _selectedValue.OnSelected += OnSelected;
+            OnSelected(_selectedValue.CurrentValue);
+        }
+        
+        private void OnSelected(ISelectable selected)
+        {
+            bool isEmpty = selected == null;
+
+            _buttonMoveGameObject.SetActive(!isEmpty);
+            _buttonAttackGameObject.SetActive(!isEmpty);
+            _buttonPatrolGameObject.SetActive(!isEmpty);
+            _buttonHoldGameObject.SetActive(!isEmpty);
+        }
+    }
+}
