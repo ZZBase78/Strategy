662d63b9e964909fa0afc9a23925a27a18a31b42
ZZBase78 <88794134+ZZBase78@users.noreply.github.com>
2022-07-03 17:56:27 +0300

    Задание №1. Сделаны остальные команды интерфейса

--- Assets/Scripts/Abstractions/Commands/CommandExecutors/AttackCommandExecutor.cs
+++ Assets/Scripts/Abstractions/Commands/CommandExecutors/AttackCommandExecutor.cs
@@ -7,7 +7,7 @@ namespace Abstractions.Commands.CommandExecutors
     {
         public override void ExecuteSpecificCommand(IAttackCommand command)
         {
-            Debug.Log($"{name} is attacking!");
+            Debug.Log($"{name} is attacking to {command.Target}!");
         }
     }
 }
--- Assets/Scripts/UserControlSystem/UI/Model/CommandCreator/PatrolCommandCommandCreator.cs
+++ Assets/Scripts/UserControlSystem/UI/Model/CommandCreator/PatrolCommandCommandCreator.cs
@@ -1,13 +1,33 @@
 ﻿using System;
 using Abstractions.Commands.CommandsInterfaces;
+using UnityEngine;
+using UserControlSystem.CommandsRealization;
+using Utils;
+using Zenject;
 
 namespace UserControlSystem
 {
     public sealed class PatrolCommandCommandCreator : CommandCreatorBase<IPatrolCommand>
     {
-        protected override void ClassSpecificCommandCreation(Action<IPatrolCommand> creationCallback)
+        [Inject] private AssetsContext _context;
+        private Action<IPatrolCommand> _creationCallback;
+
+        [Inject]
+        private void Init(Vector3Value groundClicks) => groundClicks.OnNewValue += ONNewValue;
+
+        private void ONNewValue(Vector3 groundClick)
         {
-            
+            _creationCallback?.Invoke(_context.Inject(new PatrolCommand(groundClick)));
+            _creationCallback = null;
+        }
+
+        protected override void ClassSpecificCommandCreation(Action<IPatrolCommand> creationCallback)
+            => _creationCallback = creationCallback;
+
+        public override void ProcessCancel()
+        {
+            base.ProcessCancel();
+            _creationCallback = null;
         }
     }
 }
