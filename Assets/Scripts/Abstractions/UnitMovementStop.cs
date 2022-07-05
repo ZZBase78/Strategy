e399d9eaa120b7e99a2ce2e6c28c3d229de2fea9
ZZBase78 <88794134+ZZBase78@users.noreply.github.com>
2022-07-05 19:22:13 +0300

    Сделано задание 2. Создан базовый абстрактный класс AwaiterBase

--- Assets/Scripts/Abstractions/UnitMovementStop.cs
+++ Assets/Scripts/Abstractions/UnitMovementStop.cs
@@ -7,40 +7,6 @@ namespace Core
 {
     public class UnitMovementStop : MonoBehaviour, IAwaitable<AsyncExtensions.Void>
     {
-        public class StopAwaiter : IAwaiter<AsyncExtensions.Void>
-        {
-            private readonly UnitMovementStop _unitMovementStop;
-            private Action _continuation;
-            private bool _isCompleted;
-
-            public StopAwaiter(UnitMovementStop unitMovementStop)
-            {
-                _unitMovementStop = unitMovementStop;
-                _unitMovementStop.OnStop += ONStop;
-            }
-
-            private void ONStop()
-            {
-                _unitMovementStop.OnStop -= ONStop;
-                _isCompleted = true;
-                _continuation?.Invoke();
-            }
-
-            public void OnCompleted(Action continuation)
-            {
-                if (_isCompleted)
-                {
-                    continuation?.Invoke();
-                }
-                else
-                {
-                    _continuation = continuation;
-                }
-            }
-            public bool IsCompleted => _isCompleted;
-            public AsyncExtensions.Void GetResult() => new AsyncExtensions.Void();
-        }
-
         public event Action OnStop;
 
         [SerializeField] private NavMeshAgent _agent;
