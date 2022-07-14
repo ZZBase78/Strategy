50baefb87e008c19a0c0c8114ce8cfac6ec2cd75
ZZBase78 <88794134+ZZBase78@users.noreply.github.com>
2022-07-15 02:40:22 +0300

    Загружен проект с урока 7

--- Assets/Scripts/Core/CommandExecutors/PatrolCommandExecutor.cs
+++ Assets/Scripts/Core/CommandExecutors/PatrolCommandExecutor.cs
@@ -1,16 +1,15 @@
-﻿using Abstractions.Commands;
+﻿using System.Threading.Tasks;
+using Abstractions.Commands;
 using Abstractions.Commands.CommandsInterfaces;
-using System.Threading.Tasks;
 using UnityEngine;
 
 namespace Core.CommandExecutors
 {
     public class PatrolCommandExecutor : CommandExecutorBase<IPatrolCommand>
     {
-        public override Task ExecuteSpecificCommand(IPatrolCommand command)
+        public override async Task ExecuteSpecificCommand(IPatrolCommand command)
         {
             Debug.Log($"{name} patroling!");
-            return Task.CompletedTask;
         }
     }
 }
